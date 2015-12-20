3185547f56580df45e7cb85e8d939f5d1d576cd6
Gloria <totallygloria@users.noreply.github.com>
2015-12-19 21:44:46 -0800

    Inifinite Monkeys, random hill climbing algorithm
    This time, the hill climbing changes all of the incorrect letters randomly, rather than just one at a time.
    
    The end result?  Only 80-120 iterations to get to the correct sentence.
    
    
    http://interactivepython.org/runestone/static/pythonds/Introduction/DefiningFunctions.html

--- InfiniteMonkeys.py
+++ InfiniteMonkeys.py
@@ -1,7 +1,6 @@
 # Algorithms and Data Structures: Intro
 # Infinite Monkeys Theorem
 # 12/19/15
-# @totallygloria
 
 import random, string
 
@@ -15,19 +14,18 @@ def generate_string():	# generates a random string 27 char long
 
 def check_string(current_string):
 	
-	shakespeare = "methinks it is like a weasel"
+	shakespear = "methinks it is like a weasel"
 	match = 0
-	incorrect_indices = []
+	correct_indices = []
 	
 	for i in range(28):
-		if shakespeare[i] == current_string[i]:
+		if shakespear[i] == current_string[i]:
 			match += 1
-		else:
-			incorrect_indices.append(i)
+			correct_indices.append(i)
 	
 	match_percentage = float(match) / 28 * 100
 	
-	return current_string, match_percentage, incorrect_indices
+	return current_string, match_percentage, correct_indices
 	
 
 def update_string(current_string, match_percentage, correct_indices):
@@ -45,40 +43,31 @@ def update_string(current_string, match_percentage, correct_indices):
 	
 def update_one(current_string, match_percentage, incorrect_indices):
 	
-	updated_string = ''
+	updated_string = current_string
 	
-	index = random.choice(incorrect_indices)
 	
-	for i in range(28):
-		if i != index:
-			updated_string += current_string[i]
-		else:
-			updated_string += random.choice("abcdefghijklmnopqrstuvwxyz ")
 	
-	return updated_string
 	
 	
 starting = generate_string()
 
-current_string, match_percentage, incorrect_indices = check_string(starting)
+current_string, match_percentage, correct_indices = check_string(starting)
 
-
-def do_the_thing(current_string, match_percentage, incorrect_indices):
+def do_the_thing(current_string, match_percentage, correct_indices):
 
 	total_count = 0
 
 	while match_percentage < 100:
 
-		updated_string = update_one(current_string, match_percentage, incorrect_indices)
-		current_string, match_percentage, incorrect_indices = check_string(updated_string)
+		updated_string = update_string(current_string, match_percentage, correct_indices)
+		current_string, match_percentage, correct_indices = check_string(updated_string)
 		total_count += 1
 		if total_count % 10 == 0:
-			print current_string, match_percentage
+			print current_string, match_percentage, correct_indices
 			
-	print current_string, match_percentage
-	print "Total count: ", total_count
+	print total_count
 
 	
-do_the_thing(current_string, match_percentage, incorrect_indices)
+do_the_thing(current_string, match_percentage, correct_indices)
 	
 
